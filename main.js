ed1ca62d4fdf223460fb9c2ebf1bf3dfbaa7683f
Simon Bengtsson <sillen.00@hotmail.com>
2023-01-05 23:50:19 +0100

    sats shows btc in input

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 window.addEventListener('DOMContentLoaded', main)
 
 function main() {
+    addEventListeners();
 
+}
+
+
+
+function addEventListeners(){
+    const satsInput = document.getElementById("sats");
+    satsInput.addEventListener("input", satsToBtc)
+}
+
+
+
+function satsToBtc(){
+    let satsInput = document.getElementById("sats").value
+    let btcInput = document.getElementById("btc")
     
+    btcInput.value = satsInput / 100_000_000;
+
 }
